826381065c413d2722b9925f89054a9c6e85d292
Kan-Jen Liu <kanjenuw@gmail.com>
2023-10-16 16:51:27 -0700

    complete exercise 03
    edit js and spec.js also learned how to debug with node.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let l = 0, r = string.length - 1;
+    let chars = string.split("");
+    while (l < r) {
+        tmp = chars[l];
+        chars[l] = chars[r];
+        chars[r] = tmp;
+        l++, r--;
+    }
+    return chars.join('')
 };
 
 // Do not edit below this line
