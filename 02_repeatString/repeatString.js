fa390515348f7e07a02ca2b729ec88cb80c10a3c
Kan-Jen Liu <kanjenuw@gmail.com>
2023-10-16 16:12:11 -0700

    complete exercise-02
    edit repeatString .js and spec.js file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let res = '';
+    while (n > 0) {
+        res += string;
+        n--;
+    }
+    return (n < 0) ? 'ERROR' : res
 };
 
 // Do not edit below this line
