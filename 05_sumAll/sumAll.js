bde9a8d10dec4996fe660b724d0c469f0207d00c
Kan-Jen Liu <kanjenuw@gmail.com>
2023-10-16 18:02:48 -0700

    complete exercise 05
    did not learn new tricks

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (n1, n2) {
+    if (!(typeof n1 === "number") ||
+        !(typeof n2 === "number") || n1 < 0 || n2 < 0) {
+        return 'ERROR'
+    }
+    let res = 0;
+    for (let i = Math.min(n1, n2); i < Math.max(n1, n2) + 1; i++) {
+        res += i
+    }
+    return res
 };
 
 // Do not edit below this line
