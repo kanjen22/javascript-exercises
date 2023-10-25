6f54260a09e93ff10e2789fc0765b50d2f38c4f9
Kan-Jen Liu <kanjenuw@gmail.com>
2023-10-25 14:33:34 -0700

    completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((p1, p2) => {
+        const p1Age = p1.yearOfDeath ? p1.yearOfDeath - p1.yearOfBirth : new Date().getFullYear() - p1.yearOfBirth;
+        const p2Age = p2.yearOfDeath ? p2.yearOfDeath - p2.yearOfBirth : new Date().getFullYear() - p2.yearOfBirth;
+        return p1Age > p2Age ? p1 : p2; 
+    })
 };
 
 // Do not edit below this line
