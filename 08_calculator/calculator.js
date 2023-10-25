30f77a0408d2c9672b32bd12427f64bcf9df590d
Kan-Jen Liu <kanjenuw@gmail.com>
2023-10-25 13:42:40 -0700

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+    return nums.reduce((total, num) => {
+        return total + num;
+    }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+    return nums.reduce((total, num) => {
+        return total * num;
+    }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+    let res = 1;
+    while (n > 0) {
+        res *= n;
+        n--;
+    }
+    return res
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
