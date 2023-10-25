bd411a7cc053de3d3a8edfde96b5a84525b8e385
Kan-Jen Liu <kanjenuw@gmail.com>
2023-10-25 14:15:09 -0700

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let fib = [1, 1]
+    if (n <= 2) {
+        return fib[n - 1];
+    }
+    for (let i = 2; i < n; i++) {
+        fib.push(fib[fib.length - 1] + fib[fib.length - 2])
+    }
+    return fib[fib.length - 1];
 };
 
 // Do not edit below this line
